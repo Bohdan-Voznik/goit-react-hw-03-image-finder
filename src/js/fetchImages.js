703692bfd6b12f7e6335801210e8c4e5529c1afc
Bohdan-Vozniak <bogdan2907@gmail.com>
2022-03-22 18:00:29 +0200

    Revert "fixed image object"
    This reverts commit 62d4a161e1e9e536d6e416d05b52a887ce1b9adc.

--- src/js/fetchImages.js
+++ src/js/fetchImages.js
@@ -18,5 +18,18 @@ export const fetchImagesByTag = async (tag, page) => {
   const { hits, totalHits } = serchResult.data;
   const totalPages = Math.ceil(totalHits / options.params.per_page);
 
-  return { hits, totalPages, totalHits };
+  const resultHits = hits.map(hit => {
+    return {
+      id: hit.id,
+      webformatURL: hit.webformatURL,
+      largeImageURL: hit.largeImageURL,
+      tags: hit.tags,
+      likes: hit.likes,
+      views: hit.views,
+      comments: hit.comments,
+      downloads: hit.downloads,
+    };
+  });
+
+  return { hits: resultHits, totalPages, totalHits };
 };
